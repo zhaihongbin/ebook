94239bc962fe77722d38b8fea93488cb5b91e424
zhaihongbin <zhbguoke@gmail.com>
2020-03-11 19:57:05 +0800

    阅读器之手势翻页

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -12,15 +12,48 @@ export default {
   computed: { ...mapGetters(['fileName']) },
   methods: {
     ...mapActions('book', ['setFileName']),
+    prevPage () {
+      if (this.rendition) { this.rendition.prev() }
+    },
+    nextPage () {
+      if (this.rendition) { this.rendition.next() }
+    },
+    toggleTitleAndMenu () { },
     initEpub () {
       const url = 'http://192.168.0.103:8181/epub/' + this.fileName + '.epub'
+      // 初始化epub，传入电子书地址
       this.book = new Epub(url)
-      this.book.renderTo('read', {
+      // 将电子书绑定到dom节点，并进行基础设置
+      this.rendition = this.book.renderTo('read', {
         width: innerWidth,
         height: innerHeight,
         // 实现在微信上能正常访问
         method: 'default'
-      }).display()
+      })
+      // 显示电子书
+      this.rendition.display()
+      // 监听电子书页面的手势
+      this.rendition.on('touchstart', event => {
+        // console.log(event)
+        this.touchStartX = event.changedTouches[0].clientX
+        this.touchStartTimeStamp = event.timeStamp
+      })
+      this.rendition.on('touchend', event => {
+        this.touchEndX = event.changedTouches[0].clientX
+        this.touchEndTimeStamp = event.timeStamp
+        const offsetX = this.touchEndX - this.touchStartX
+        const timeInterval = this.touchEndTimeStamp - this.touchStartTimeStamp
+        console.log(offsetX, timeInterval)
+        if (timeInterval < 500 && offsetX > 40) {
+          this.prevPage()
+        } else if (timeInterval < 500 && offsetX < -40) {
+          this.nextPage()
+        } else {
+          this.toggleTitleAndMenu()
+        }
+        // event.preventDefault()
+        event.stopPropagation()
+      })
     }
   },
   created () {
